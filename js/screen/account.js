b2eab64d20a8cd19b961991b65035744b7c91842
VPmotohira <tatsuro.motohira@vantage-point.co.jp>
2025-09-28 18:54:39 +0900

    Update account.js

--- js/screen/account.js
+++ js/screen/account.js
@@ -1,3 +1,12 @@
-export default function renderAccountScreen(container) {
-    container.innerHTML = `<div class="card"><h2>アカウント</h2><p>開発中です。</p></div>`;
+// account.js
+
+function renderAccountScreen(container) {
+    container.innerHTML = `
+        <div class="card bg-white p-5 rounded-lg shadow">
+            <h2 class="text-xl font-bold mb-3">アカウント</h2>
+            <p class="text-sm text-gray-500">サインアップ不要で利用できます</p>
+        </div>
+    `;
 }
+
+export default renderAccountScreen;
